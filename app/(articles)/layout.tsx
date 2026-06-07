00324d59b617263efaa4051fce291913ce37b81a
blsage <ben@unionst.com>
2026-06-07 10:53:46 -0700

    Remove article fade-in; posts appear instantly like benji.org

--- app/(articles)/layout.tsx
+++ app/(articles)/layout.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { ArticleAside } from "./article-aside";
 import { ArticleHeader } from "./article-header";
-import { ArticleTransition } from "./article-transition";
 import styles from "./article.module.css";
 
 export default function ArticleLayout({
@@ -10,7 +9,7 @@ export default function ArticleLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ArticleTransition>
+    <>
       <ArticleAside />
       <article className="article">
         <ArticleHeader />
@@ -21,6 +20,6 @@ export default function ArticleLayout({
           </Link>
         </footer>
       </article>
-    </ArticleTransition>
+    </>
   );
 }
