28a0c3c017ab981ff4e5c2139ece517095f7497c
blsage <ben@unionst.com>
2026-06-06 21:38:54 -0700

    Match favicon: dark dot on transparent

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from "next/og";
-import { site } from "@/site.config";
 
-export const size = { width: 64, height: 64 };
+export const size = { width: 139, height: 139 };
 export const contentType = "image/png";
 
 export default function Icon() {
@@ -14,14 +13,17 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#111",
-          color: "#fdfdfc",
-          borderRadius: 14,
-          fontSize: 38,
-          fontWeight: 600,
+          background: "transparent",
         }}
       >
-        {site.name.charAt(0)}
+        <div
+          style={{
+            width: 48,
+            height: 48,
+            borderRadius: "50%",
+            background: "#111",
+          }}
+        />
       </div>
     ),
     size
